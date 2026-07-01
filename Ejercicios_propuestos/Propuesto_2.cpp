439a2d9e075015ac7e5cbb6f17dbade3c604c7cc
csapazah <csapazah@unjbg.edu.pe>
2026-06-30 21:25:24 -0500

    Lectura de datos de cada persona

--- Ejercicios_propuestos/Propuesto_2.cpp
+++ Ejercicios_propuestos/Propuesto_2.cpp
@@ -12,6 +12,18 @@ int main(){
 	cout << "Nro de personas " << endl;
 	cin >> n;
     cin.ignore();
-    
+
+    for(int i=0 ; i<n ; i++){
+		cout << "Persona " << i+1 << endl;
+		cout << "Nombre " << endl;
+		cin.getline(Identidad[i].nombres,50);
+		cout << "Nro de DNI " << endl;
+		cin.getline(Identidad[i].DNI,10);
+		cout << "Edad " << endl;
+		cin >> Identidad[i].edad;
+		cin.ignore();
+		cout << "====================================" << endl;
+	}
+
     return 0;
 }
